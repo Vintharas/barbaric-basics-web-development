d09712d72f586cd41e0e19ba60c09462a0d6f90f
Jaime <jaime.gg83@gmail.com>
2015-02-11 07:40:59 +0100

    Factored js code:
    - Separated DOM manipulation from business logic
    - Did some renaming

---
+++ iWrite/app/scripts/iwrite.dom.js
@@ -0,0 +1,112 @@
+﻿(function (iwrite) {
+    'use strict';
+    var DOM = {
+            loadFilesInSidebar: loadFilesInSidebar,
+            setFileListItemAsActive: setFileListItemAsActive,
+            getOrCreateEditor: getOrCreateEditor,
+            getOrCreatePreview: getOrCreatePreview,
+            setPreviewContent: setPreviewContent
+        };
+
+    iwrite.DOM = DOM;
+    
+    function loadFilesInSidebar(/* fileNames, onClickFile */) {
+        var sidebarList = document.querySelector('.sidebar div.list-group'),
+            filesToLoad,
+            onClickFile;
+
+        if (arguments.length === 2) {
+            if (typeof arguments[0] === 'object') {
+                filesToLoad = arguments[0];
+            } else if (typeof arguments[0] === 'string') {
+                filesToLoad = [arguments[0]];
+            } else {
+                throw new Error('Wrong typed arguments. First argument should be a fileName or an array of fileNames');
+            }
+            onClickFile = arguments[1];
+        } 
+
+        filesToLoad.forEach(loadFileInSidebar);
+        setFirstItemAsActive();
+
+        function loadFileInSidebar(fileName) {
+            var a = document.createElement('a');
+            a.innerHTML = fileName;
+            a.className = 'list-group-item';
+            a.href = '#';
+            a.onclick = onClickFile;
+            sidebarList.appendChild(a);
+        }
+
+        function setFirstItemAsActive() {
+            var children = sidebarList.childNodes;
+            if (children.length > 0) {
+                onClickFile.call(children[0]);
+            }
+        }
+    }
+
+    function setFileListItemAsActive(element) {
+        var sidebarList = document.querySelector('.sidebar div.list-group');
+        var children = sidebarList.childNodes;
+        children = Array.prototype.slice.call(children);
+        children.forEach(function (child) {
+            if (element === child) {
+                child.className = 'list-group-item active';
+            }
+            child.className = 'list-group-item';
+        });
+    }
+
+    function getOrCreateEditor(onReloadPreview) {
+        var div,
+            textArea,
+            content;
+
+        textArea = document.querySelector('div.editor textarea');
+        if (textArea) {
+            return textArea;
+        }
+
+        div = document.createElement('div');
+        div.className = 'col-md-6 col-sm-6 editor';
+        textArea = document.createElement('textarea');
+        textArea.className = 'form-control noresize';
+        textArea.noResize = true;
+        textArea.rows = 10;
+        textArea.addEventListener('keypress', createReloadPreview(onReloadPreview));
+        div.appendChild(textArea);
+        content = document.querySelector('div.content');
+        content.appendChild(div);
+        return textArea;
+    }
+
+    function createReloadPreview(onReloadPreview) {
+        return function reloadPreview() {
+            var editor = document.querySelector('div.editor textarea');
+            onReloadPreview(editor.value);
+        }
+    }
+
+    function getOrCreatePreview() {
+        var div,
+            content;
+
+        div = document.querySelector('div.preview');
+        if (div) {
+            return div;
+        }
+
+        div = document.createElement('div');
+        div.className = 'col-md-6 col-sm-6 preview basic-borders';
+        content = document.querySelector('div.content');
+        content.appendChild(div);
+        return div;
+    }
+
+    function setPreviewContent(parsedHtml) {
+        var div = document.querySelector('div.preview');
+        div.innerHTML = parsedHtml;
+    }
+
+}(window.iwrite = window.iwrite || {}));
---
+++ iWrite/app/scripts/iwrite.js
@@ -0,0 +1,83 @@
+﻿(function (iwrite) {
+    'use strict';
+    var fileNames,
+        KEY_FILENAMES = 'fileNames',
+        DOM = iwrite.DOM,
+        markdownParser = new Worker('scripts/markdownParser.js');
+
+    markdownParser.onmessage = handleParsedHtml;
+
+    window.addEventListener('load', initialize);
+    
+    function initialize() {
+        var addImageBtn = document.getElementById('add-image');
+        addImageBtn.addEventListener('click', addImage);
+        var addFileBtn = document.getElementById('add-file');
+        addFileBtn.addEventListener('click', addFile);
+        loadExistingFiles( );
+    }
+
+    function addImage() {
+       // add image and paint with canvas and svg or something like that 
+    }
+
+    function loadExistingFiles() {
+        fileNames = iwrite.localStorage.getItem(KEY_FILENAMES) || [];
+        DOM.loadFilesInSidebar(fileNames, /* onClickFile */ openFile);
+    }
+
+    function addFile() {
+        var newFileNameTemplate = 'New File ',
+            newFileName = 'New File 1',
+            counter = 1;
+        while (filenameAlreadyExists(newFileName)) {
+            counter++;
+            newFileName = newFileNameTemplate + counter.toString();
+        }
+
+        fileNames.push(newFileName); // TODO: sort alphabetically
+
+        iwrite.localStorage.setItem(KEY_FILENAMES, fileNames);
+        iwrite.localStorage.setItem(newFileName, {name: newFileName, content: 'Start writing here in markdown...'});
+
+        DOM.loadFilesInSidebar(newFileName, /* onClickFile */ openFile);
+
+        function filenameAlreadyExists(newFileName) {
+            return fileNames.some(function(fileName) { return newFileName === fileName; });
+        }
+    }
+
+    function openFile() {
+        var fileName = this.text;
+        DOM.setFileListItemAsActive(this);
+        this.className = 'list-group-item active';
+        console.log('opening file: ' + fileName);
+        var file = iwrite.localStorage.getItem(fileName);
+        loadContent(file.content);
+        loadPreview(file.content);
+    }
+
+    function loadContent(fileContent) {
+        console.log('Open editor with content: ' + fileContent);
+        var editor = DOM.getOrCreateEditor(/* onReloadPreview */ computePreview);
+        editor.value = fileContent;
+    }
+
+    function loadPreview(fileContent) {
+        DOM.getOrCreatePreview();
+        computePreview(fileContent);
+    }
+
+    function computePreview(fileContent) {
+        console.log('sending data to worker: ', fileContent);
+        markdownParser.postMessage(fileContent);
+    }
+
+    function handleParsedHtml(e) {
+        var parsedHtml = e.data;
+        console.log('received data from worker: ', parsedHtml);
+        DOM.setPreviewContent(parsedHtml);
+    }
+
+}(window.iwrite = window.iwrite || {}));
+
